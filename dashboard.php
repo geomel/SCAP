956765c61763289794717683b14478054c1fcfda
geomel <geomel@gmail.com>
2022-04-08 19:43:41 +0300

    Load top page data

--- dashboard.php
+++ dashboard.php
@@ -22,3 +22,27 @@ include("inc/header.php");
 $page_nav["dashboard"]["sub"]["social"]["active"] = true;
 include("inc/nav.php");
 ?>
+
+    <!-- MAIN CONTENT -->
+    <div id="content">
+        <div class="row">
+            <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+                <h1 class="page-title txt-color-blueDark"><i class="fa-fw fa fa-home"></i> SC Dashboard </h1>
+            </div>
+            <div class="col-xs-12 col-sm-5 col-md-5 col-lg-8">
+                <ul id="sparks" class="">
+                    <li class="sparks-info">
+                        <h5> Total Contracts Loaded <span class="txt-color-blue"><i class="fa fa-cloud-download" data-rel="bootstrap-tooltip" title="Versions"></i>&nbsp;&nbsp<?php echo count($_SESSION["adr"]); ?></span></h5>
+                    </li>
+                  <li class="sparks-info">
+                        <h5> Total Contracts Analyzed <span class="txt-color-blue"><i class="fa fa-database" data-rel="bootstrap-tooltip" title="Versions"></i>&nbsp;&nbsp<?php echo "103251"; ?></span></h5>
+                    </li>
+                 <!--     
+                    <li class="sparks-info">
+                        <h5> Git path <span class="txt-color-purple"><i class="fa fa-code" data-rel="bootstrap-tooltip" title="Git Path"></i>&nbsp&nbsp<?php echo "<a href='" . $_SESSION["githubpath"] . "' target='_blank'>" . $_SESSION["githubpath"] . "</a>"; ?></span></h5>
+                    </li>
+                -->
+                </ul>
+            </div>
+        </div>
+ <!--
