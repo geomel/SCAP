8b0885df95d48f5c9622dbbdd9b2afbd7cd2c45b
geomel <geomel@gmail.com>
2022-04-08 19:21:01 +0300

    Initial commit

---
+++ filenames.py
@@ -0,0 +1,3 @@
+from os import listdir
+file_list = listdir("/Applications/AMPPS/www/full/t")
+print("\n".join(file_list))
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.6.4',
+            'version' => '1.6.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'reference' => 'fbe3fe878f4fe69048bb8a52783a09802004f548',
+            'dev_requirement' => false,
+        ),
+    ),
+);
